c041646968f2ce5d96bc6629ec2ca201d03051c7
Amit Kumar Yadav <amit@Amits-MacBook-Pro.local>
2022-04-03 23:00:00 +0530

    add hello world exercise and solutions

---
+++ 1 Hello World/hello.cpp
@@ -0,0 +1,19 @@
+// this is comment
+// lkfhaosdhfo
+/*
+sadfasdfl aosdfhoiasd f
+dsflnsadf
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std; // constant
+
+int main()
+{
+	cout << "Hello Mr. X" << endl;
+    cout << "Coders" << endl;
+
+    return 0;
+}
---
+++ 1 Hello World/solutions.cpp
@@ -0,0 +1,43 @@
+// Part 1
+// Exercises for this session:
+// 1. WAP to print name on two lines, 
+// Hello
+// World!
+
+// #### solution
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello " << endl;
+//     cout << "World!" << endl;
+
+//     return 0;
+// }
+
+// 2. WAP to print instructions for going from your room to outside in your home
+// Standup
+// walk 10 steps east
+// turn 135* right
+// walk straight 6 steps
+// trun left 45
+// walk 4 steps
+// move 4 steps down the stairs
+// outside!
+
+// #### solution
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Standup" << endl;
+//     cout << "walk 10 steps east" << endl;
+//     cout << "turn 135* right" << endl;
+//     cout << "walk straight 6 steps" << endl;
+//     cout << "trun left 45" << endl;
+//     cout << "walk 4 steps" << endl;
+//     cout << "move 4 steps down the stairs" << endl;
+//     cout << "outside!" << endl;
+
+//     return 0;
+// }
