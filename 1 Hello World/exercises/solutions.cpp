bffc453c516f842fb9e9cb78f056074adc081b42
Amit Kumar Yadav <hi.codingonmars@gmail.com>
2022-04-04 01:05:36 +0530

    reformat solutions

---
+++ 1 Hello World/exercises/solutions.cpp
@@ -0,0 +1,41 @@
+// Part 1
+// 1. WAP to print below strings, 
+// Hello India!
+// Happy Coding /\:)
+
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello India!\nHappy Coding /\\:)";
+    return 0;
+}
+*/
+
+// 2. WAP to print instructions for going from your room to outside in your home
+// 	Standup
+// 	walk 10 steps of 1feet each  east
+// 	turn 135 degree right
+// 	walk straight 6 steps
+// 	trun left 45 degree
+// 	walk 4 steps
+// 	move 4 steps down the stairs
+// 	outside!
+
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Standup\n";
+    cout << "walk 10 steps east";
+    cout << "turn 135* right";
+    cout << "walk straight 6 steps";
+    cout << "trun left 45";
+    cout << "walk 4 steps";
+    cout << "move 4 steps down the stairs";
+    cout << "outside!";
+    return 0;
+}
+*/
